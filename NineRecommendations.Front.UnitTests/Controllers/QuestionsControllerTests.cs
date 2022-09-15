32e239f00053cbbf06ee2f26703c458eae64048d
Tomasz Czarnecki <creo.one@gmail.com>
2022-09-15 17:08:22 +0200

    Added integration tests for QuestionsController

--- NineRecommendations.Front.UnitTests/Controllers/QuestionsControllerTests.cs
+++ NineRecommendations.Front.UnitTests/Controllers/QuestionsControllerTests.cs
@@ -2,15 +2,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using NineRecommendations.Core.Questionnaires;
+using NineRecommendations.Core.Questionnaires.Answers;
+using NineRecommendations.Core.Questionnaires.Questions;
 using NineRecommendations.Front.Controllers;
+using NineRecommendations.Front.Models;
 using NSubstitute;
+using System.Text;
 
 namespace NineRecommendations.Front.UnitTests.Controllers
 {
     public class QuestionsControllerTests
     {
         [Fact]
-        public void AskActionWithHttpGetShouldAndNoQuestionnaireIdShouldRedirect()
+        public void AskActionWithHttpGetAndNoQuestionnaireIdShouldRedirect()
         {
             // arrange
 
@@ -31,6 +35,35 @@ namespace NineRecommendations.Front.UnitTests.Controllers
             // assert
 
             Assert.Equal("Recommendations", actual.ControllerName);
+        }
+
+        [Fact]
+        public void AskActionWithHttpGetReturnCorrectViewModel()
+        {
+            // arrange
+
+            var expected = "Question?";
+            var questionnaireManipulator = Substitute.For<IQuestionnaireManipulator>();
+            questionnaireManipulator.GetQuestionById(Arg.Any<Guid>()).Returns(new DefaultQuestion(Guid.NewGuid(), expected, Array.Empty<IAnswer>()));
+
+            var controller = new QuestionsController(questionnaireManipulator);
+            controller.ControllerContext = new ControllerContext();
+            var tempData = Substitute.For<ITempDataDictionary>();
+            controller.TempData = tempData;
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            var session = Substitute.For<ISession>();
+            session.TryGetValue(Arg.Any<string>(), out Arg.Any<byte[]?>()).Returns(call => { call[1] = Encoding.UTF8.GetBytes(Guid.NewGuid().ToString()); return true; });
+            controller.ControllerContext.HttpContext.Session = session;
+
+            // act
+
+            var result = (ViewResult)controller.Ask(Guid.NewGuid());
+
+            // assert
+
+            var actual = result.Model as QuestionModel;
+            Assert.Equal(expected, actual?.Content);
         }
     }
 }
